86c5eaee062666eb30fc98d1da370903e122d623
Daneesh <mustafadaneesh@gmail.com>
2020-10-03 00:21:54 +0530

    [Daneesh] Refactor : Checking the equality of 2 lines

--- LineCompare/LineCompare/Program.cs
+++ LineCompare/LineCompare/Program.cs
@@ -6,19 +6,28 @@ namespace LineCompare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch !!");
-            Console.WriteLine("\n");
-            Console.WriteLine("Enter the initial x & y co-ordinate of the line : ");
+            Console.WriteLine("Enter the initial x & y co-ordinate of 1st line : ");
             double x1 = Convert.ToDouble(Console.ReadLine());
             double y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the final x & y co-ordinate of the line :");
+            Console.WriteLine("Enter the final x & y co-ordinate of 1st line :");
             double x2 = Convert.ToDouble(Console.ReadLine());
             double y2 = Convert.ToDouble(Console.ReadLine());
-            double diffX = x2 - x1;
-            double diffY = y2 - y1;
-            double lengthOfLine = Math.Sqrt(Math.Pow(diffX, 2) + Math.Pow(diffY, 2));
-            Console.WriteLine("\n");
-            Console.WriteLine("Length of the line : " + lengthOfLine);
+            double diffX1 = x2 - x1;
+            double diffY1 = y2 - y1;
+            double length1 = Math.Sqrt(Math.Pow(diffX1, 2) + Math.Pow(diffY1, 2));
+            Console.WriteLine("Enter the initial x & y co-ordinate of 2nd line : ");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            double y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the final x & y co-ordinate of 2nd line :");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            double y4 = Convert.ToDouble(Console.ReadLine());
+            double diffX2 = x4 - x3;
+            double diffY2 = y4 - y3;
+            double length2 = Math.Sqrt(Math.Pow(diffX2, 2) + Math.Pow(diffY2, 2));
+            if (length1.Equals(length2))
+                Console.WriteLine("Lengths of both lines ARE EQUAL");
+            else
+                Console.WriteLine("Lengths of both lines ARE NOT EQUAL");
         }
     }
 }
